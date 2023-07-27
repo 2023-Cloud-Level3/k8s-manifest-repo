317351248eda082e985f84f313ae6488546ae834
riverjin839 <109583750+riverjin839@users.noreply.github.com>
2023-07-27 16:18:08 +0900

    Add files via upload

---
+++ argocd-add-repo.sh
@@ -0,0 +1,2 @@
+argocd repo add https://git-codecommit.ap-northeast-2.amazonaws.com/v1/repos/test-repo-source --name team01-git --username git-user-at-883545701064  --password YRCo6ywaGYdwAHADr9Xdy7AKlC3bVi2FItLH8GJawCQ=    --insecure-skip-server-verification
+
